dea56e70035f89d42033047adfb20ff481818d4f
Mohammadreza Hashemian <mr.hashemian2000@gmail.com>
2022-12-24 17:47:40 +0330

    Add cache message and localstorage for caching

--- git_profile_request.js
+++ git_profile_request.js
@@ -6,82 +6,97 @@ const date_options = {weekday: 'long', year: 'numeric', month: 'long', day: 'num
 
 document.getElementById("submit").addEventListener("click", function () {
     let userName = document.getElementById("username").value
-    // let result;
+    document.getElementById("box-message").innerHTML = ""
+
     if (userName == null || userName === "") {
-        showMessage("username coudnt be empty")
+        showMessage("username could not be empty")
+    } else {
+        let profileCache = localStorage.getItem("profile:" + userName)
+        console.log("profile cache", JSON.parse(profileCache))
+        if (profileCache !== null) {
+            document.getElementById("box-message").innerHTML += "this profile got from local storage"
+            renderGitProfile(JSON.parse(profileCache))
+            getUserLanguage(userName)
+        } else {
+            getGitProfile(userName)
+        }
+    }
+})
+
+function getUserLanguage(userName) {
+    let languageCache = localStorage.getItem("language:" + userName)
+    console.log("language cache", languageCache)
+    if (languageCache !== null) {
+        document.getElementById("box-message").innerHTML += "<br>this skills got from local storage"
+        languageCache = JSON.parse(languageCache)
+        let d22 = document.getElementById("d22")
+        for (let i = 0; i < languageCache.length; i++) {
+            let temp = document.createElement("p")
+            temp.innerHTML = languageCache[i][0] + ": " + languageCache[i][1]
+            d22.appendChild(temp)
+        }
     } else {
         findUserLanguages(userName)
     }
-    //     let cache = localStorage.getItem(userName)
-    //     console.log("cache", cache)
-    //     if (cache !== null) {
-    //         renderGitProfile(cache)
-    //     } else {
-    //         try {
-    //             result = getGitProfile(userName).then(result => result.json())
-    //         } catch (err) {
-    //             showMessage(err)
-    //             // showMessage("failed to request github for profile")
-    //         }
-    //         // result = getGitProfile(userName)
-    //         findUserLanguages(userName)
-    //         console.log("res", result)
-    //         if (result != null) {
-    //             renderGitProfile(result)
-    //         }
-    //     }
-    //
-    // }
-});
+}
 
+function mostLanguages(array, userName) {
+    let languages = new Map();
+    for (let i = 0; i < array.length; i++) {
+        for (let lang in array[i]) {
+            if (languages.has(lang) && array[i][lang] > languages.get(lang)) {
+                languages.set(lang, array[i][lang])
+            } else {
+                languages.set(lang, array[i][lang])
+            }
+        }
+    }
 
-function findMostLanguages(resultmm) {
-    // let languages = new Map();
-    console.log("this is r", resultmm, resultmm[1])
-    // for (let i = 0; i < resultmm.length; i++) {
-    //     for (let lang in resultmm[i]) {
-    //         if (languages.has(lang) && resultmm[i][lang] > languages.get(lang)) {
-    //             languages.set(lang, resultmm[i][lang])
-    //         } else {
-    //             languages.set(lang, resultmm[i][lang])
-    //         }
-    //     }
-    // }
-    //
-    // for (let langs in result) {
-    //
-    // }
+    let sortedLanguages = new Map([...languages].sort((a, b) => b[1] - a[1]))
+    sortedLanguages = Array.from(sortedLanguages).slice(0, Math.min(3, sortedLanguages.size))
+    sortedLanguages = new Map(sortedLanguages)
 
-    // console.log("array", languages)
-    // let sortedLanguages = new Map([...languages].sort((a, b) => b[1] - a[1]))
-    // console.log("sortedmap", sortedLanguages)
-    // sortedLanguages = Array.from(sortedLanguages).slice(0, 3)
-    // sortedLanguages = new Map(sortedLanguages)
-    // console.log("sorted", sortedLanguages)
+    if (sortedLanguages.size > 0) {
+        localStorage.setItem("language:" + userName, JSON.stringify(Array.from(sortedLanguages.entries())))
+        let d22 = document.getElementById("d22")
+        console.log(sortedLanguages, "skl")
+        sortedLanguages.forEach((value, key) => {
+            let temp = document.createElement("p")
+            temp.innerHTML = key + ": " + value
+            d22.appendChild(temp)
+        });
+    }
 }
 
 function findUserLanguages(userName) {
-    fetch(`https://api.github.com/users/${userName}/repos`, requestOptions)
-        .then(response => response.json())
-        .then(result => {
-            const mresult = result.slice(0, 5)
-            mresult.forEach(findRepoLanguages)
-            findMostLanguages(mresult)
-            console.log(mresult, "result")
-        })
-        .catch(error => console.log('error', error));
-}
+    const newResult = [];
 
-function findRepoLanguages(value, index, array) {
-    // let repoName = array[index].full_name
-    fetch(`https://api.github.com/repos/${value.full_name}/languages`, requestOptions)
-        .then(response => response.json())
-        .then(result => {
-            console.log(result)
-            array[index] = result
-            // renderGitProfile(result)
+    fetch(`https://api.github.com/users/${userName}/repos`, requestOptions)
+        .then(response => {
+            if (response.status === 403) {
+                showMessage("API rate limit exceeded")
+            }
+            response.json().then(async res => {
+                try {
+                    for (let i = 0; i < 5 && i < res.length; i++) {
+                        const response = await fetch(`https://api.github.com/repos/${res[i].full_name}/languages`, requestOptions)
+                        if (response.status === 403) {
+                            showMessage("API rate limit exceeded")
+                            break
+                        }
+                        const result = await response.json()
+                        newResult.push(result)
+                    }
+                } catch (err) {
+                    showMessage("API rate limit exceeded")
+                }
+
+                mostLanguages(newResult, userName)
+            })
         })
-        .catch(error => console.log('error', error));
+        .catch(error => {
+            showMessage(error)
+        });
 }
 
 function showMessage(error) {
@@ -92,55 +107,48 @@ function showMessage(error) {
 }
 
 function getGitProfile(userName) {
-    return new Promise((resolve, reject) => (
-        fetch(`https://api.github.com/users/${userName}`, requestOptions)
-            .then(data => {
-                if (data.ok) {
-                    data.json().then(res => {
-                        resolve(res)
-                    })
-                } else if (data.status === 404) {
-                    showMessage("username not found")
-                } else if (data.status === 403) {
-                    showMessage("API rate limit exceeded")
-                }
-            })
-            .catch(err => reject(err))
-    ))
+    fetch(`https://api.github.com/users/${userName}`, requestOptions)
+        .then((response) => {
+            if (response.ok) {
+                response.json().then(result => {
+                    localStorage.setItem("profile:" + userName, JSON.stringify(result))
+                    renderGitProfile(result)
+                    getUserLanguage(userName)
+                })
+            } else if (response.status === 404) {
+                showMessage("username not found")
+            } else if (response.status === 403) {
+                showMessage("API rate limit exceeded")
+            }
+        })
+        .catch(error => {
+            showMessage(error)
+        });
+}
 
+function prepareProfileCard() {
+    document.getElementById("message").style.opacity = "0"
+    document.getElementById("profile-information").style.opacity = "1"
 
-    // return fetchData
-    // async function getData() {
-    //     const data = await fetchData()
-    //     console.log(data);
-    // }
-    //
-    // getData()
-    // fetch(`https://api.github.com/users/${userName}`, requestOptions)
-    //     .then((response) => {
-    //         if (response.ok) {
-    //             response.json().then(result => {
-    //                 console.log(result)
-    //                 return result
-    //             })
-    //         } else if (response.status === 404) {
-    //             showMessage("username not found")
-    //         } else if (response.status === 403) {
-    //             showMessage("API rate limit exceeded")
-    //         }
-    //     })
-    //     .catch(error => {
-    //         showMessage(error)
-    //     });
+    let h1Fields = document.getElementById("profile-information").getElementsByTagName("h1")
+    for (let i = 0; i < h1Fields.length; i++) {
+        h1Fields[i].innerHTML = ""
+    }
 
+    document.getElementById("bio").innerHTML = ""
+    let d22 = document.getElementById("d22")
+    let child = d22.lastElementChild
+    while (child) {
+        d22.removeChild(child);
+        child = d22.lastElementChild;
+    }
 }
 
 function renderGitProfile(profile) {
+    prepareProfileCard()
+
     let updated_at = new Date(profile.updated_at).toLocaleDateString("en-US", date_options)
     let created_at = new Date(profile.created_at).toLocaleDateString("en-US", date_options)
-
-    document.getElementById("message").style.opacity = "0"
-    document.getElementById("profile-information").style.opacity = "1"
 
     document.getElementById("profile").getElementsByTagName("img")[0].src = profile.avatar_url
     document.getElementById("name").innerHTML = profile.name
@@ -166,9 +174,5 @@ function renderGitProfile(profile) {
     }
     if (profile.bio != null) {
         document.getElementById("bio").innerHTML = profile.bio
-    }
-    if (profile.twitter_username != null) {
-        document.getElementById("profile-social-links").style.display = `block`
-        document.getElementById("profile-social-links").href = `https://twitter.com/${profile.twitter_username}`
     }
 }
